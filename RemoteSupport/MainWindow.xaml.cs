1e29f1521c66a90c9578d9201fa55038549efd2b
Radu Albastroiu <albastroiuradug@gmail.com>
2019-08-28 23:56:19 +0300

    Take screenshot

--- RemoteSupport/MainWindow.xaml.cs
+++ RemoteSupport/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,22 +13,44 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestStack.White;
 
 namespace RemoteSupport
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void GetScreenShoot(object sender, RoutedEventArgs e)
-        {
-            
-        }
+      InitializeComponent();
     }
+
+    private void GetScreenShoot(object sender, RoutedEventArgs e)
+    {
+      ScreenCapture sc = new ScreenCapture();
+
+      var screenCapture = sc.CaptureScreenShot();
+      var screenCaptureSource = BitmapToImageSource(screenCapture);
+
+      this.ScreenShot.Source = screenCaptureSource;
+    }
+
+    private BitmapImage BitmapToImageSource(Bitmap bitmap)
+    {
+      using (System.IO.MemoryStream memory = new System.IO.MemoryStream())
+      {
+        bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+        memory.Position = 0;
+        BitmapImage bitmapimage = new BitmapImage();
+        bitmapimage.BeginInit();
+        bitmapimage.StreamSource = memory;
+        bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+        bitmapimage.EndInit();
+
+        return bitmapimage;
+      }
+    }
+  }
 }
