2d0ef1aa69a21ede7bc9bd2fc229e41536b07e7e
Radu Albastroiu <albastroiuradug@gmail.com>
2019-08-29 01:04:11 +0300

    Get screen shot of an area of the screen

--- RemoteSupport/MainWindow.xaml.cs
+++ RemoteSupport/MainWindow.xaml.cs
@@ -1,25 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using TestStack.White;
+using System.Runtime.InteropServices;
 
 namespace RemoteSupport
 {
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
+  /// 
+  class User32
+  {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Rect
+    {
+      public int left;
+      public int top;
+      public int right;
+      public int bottom;
+    }
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+  }
+
   public partial class MainWindow : Window
   {
     public MainWindow()
@@ -29,12 +35,18 @@ namespace RemoteSupport
 
     private void GetScreenShoot(object sender, RoutedEventArgs e)
     {
-      ScreenCapture sc = new ScreenCapture();
+      var proc = System.Diagnostics.Process.GetProcessesByName("advinst")[0];
+      var rect = new User32.Rect();
+      User32.GetWindowRect(proc.MainWindowHandle, ref rect);
 
-      var screenCapture = sc.CaptureScreenShot();
-      var screenCaptureSource = BitmapToImageSource(screenCapture);
+      int width = rect.right - rect.left;
+      int height = rect.bottom - rect.top;
 
-      this.ScreenShot.Source = screenCaptureSource;
+      var bmp = new Bitmap(width, height);
+      Graphics graphics = Graphics.FromImage(bmp);
+      graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new System.Drawing.Size(width, height), CopyPixelOperation.SourceCopy);
+
+      this.ScreenShot.Source = BitmapToImageSource(bmp);
     }
 
     private BitmapImage BitmapToImageSource(Bitmap bitmap)
