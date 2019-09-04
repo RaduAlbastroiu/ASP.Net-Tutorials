8681f4c395161fafa08b96f54867afae090bdd92
Radu Albastroiu <radu.albastroiu@caphyon.com>
2019-09-04 16:18:14 +0300

    Add api

---
+++ Treehouse Asp.net Web Api/Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Models;
+
+namespace Treehouse.FitnessFrog.Spa.Controllers
+{
+  public class EntriesController : ApiController
+  {
+    [HttpGet]
+    public IEnumerable<Entry> Get()
+    {
+      var activityBiking = new Activity() { Name = "Biking" };
+      return new List<Entry>()
+      {
+        new Entry(2017, 1, 2,activityBiking, 10.0m),
+        new Entry(2017, 1, 2,activityBiking, 12.0m),
+        new Entry(2017, 1, 2,activityBiking, 15.0m)
+      };
+    }
+
+    public Entry Get(int id)
+    {
+      return null;
+    }
+
+    [HttpPost]
+    public void Post()
+    {
+
+    }
+
+    [HttpPut]
+    public void Put()
+    {
+
+    }
+
+    [HttpDelete]
+    public void Delete()
+    {
+
+    }
+  }
+}
